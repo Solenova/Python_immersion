db1885cf0f001ee25839218e0bd4ae7799c2a13a
OlgaS <roga&kopita@mail.ru>
2023-07-16 16:42:44 +0500

    dz1_task_8_variant2

--- task_9_multiplication_table.py
+++ task_9_multiplication_table.py
@@ -2,8 +2,18 @@
 # Выведите в консоль таблицу умножения от 2х2 до 9х10 как на школьной тетрадке.
 
 
-for i in range(0, 10):
-    for j in range(0, 11):
-        print(i, '*', j , '=', i*j)
+# for i in range(0, 10):
+#     for j in range(0, 11):
+#         print(i, '*', j , '=', i*j)
+#
+#     print("")
 
-    print("")
+# ------------2
+
+for i in range(1, 10):
+    for j in range(0, 5):
+        print(f"{j}* {i} = {i*j}\t", end="")
+    print("\t", end="")
+    for j in range(6, 10):
+        print(f"{j}* {i} = {i*j}\t", end="")
+    print()
